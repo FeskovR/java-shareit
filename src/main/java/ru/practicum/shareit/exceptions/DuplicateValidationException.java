83beab37699d2cc06fe0a640b7f02db4247226c4
FeskovR <feskovrv@yandex.ru>
2023-06-20 22:29:49 +0300

    fix: добавить тесты

--- src/main/java/ru/practicum/shareit/exceptions/DuplicateValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class DuplicateValidationException extends RuntimeException {
-    public DuplicateValidationException(String message) {
-        super(message);
-    }
-}
